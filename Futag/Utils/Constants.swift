7690958c02fa4f811399f23ed24e8baf114e12b0
Furkan Erdoğan <edorfs.fe@gmail.com>
2022-01-09 14:31:42 +0300

    Project started

--- Futag/Utils/Constants.swift
+++ Futag/Utils/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 import Firebase
 
 let STORAGE_REF = Storage.storage().reference()
-let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("images")
 
 let DB_REF = Database.database().reference()
 let REF_USERS = DB_REF.child("users")
